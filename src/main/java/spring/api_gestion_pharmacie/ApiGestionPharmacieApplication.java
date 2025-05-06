fc362885042a7027ba917e5724ab0bab039b1c9e
stouf <ouadad.mustapha01@gmail.com>
2025-05-06 17:07:43 +0100

    creer entities et lier a base donnes

---
+++ src/main/java/spring/api_gestion_pharmacie/ApiGestionPharmacieApplication.java
@@ -0,0 +1,13 @@
+package spring.api_gestion_pharmacie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionPharmacieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionPharmacieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/api_gestion_pharmacie/ApiGestionPharmacieApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.api_gestion_pharmacie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionPharmacieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
